3298ca11006e3ccb647ab28e01ff84f016f0eff4
Kaung Myat Zaw <zaw@purdue.edu>
2021-06-28 19:11:15 +0630

    Initializing files and functions for getting a random Youtube video

---
+++ src/utils/youtube.js
@@ -0,0 +1,13 @@
+// Currently implementing three ways to get videos. Each has a equal chance to get selected.
+// (1) Getting a random video from a randomly selected top-100 youtuber.
+// (2) Getting a random video by using a random word generator.
+// (3) Getting a random video from a randomly selected top Google search term.
+
+const getVideoFromRandomWord = () => {};
+
+const getVideoFromYoutuber = () => {};
+
+const getVideoFromGoogleSearch = () => {};
+
+// Randomly select one of the above three methods and use it to return a video.
+const getRandomVideo = () => {};
