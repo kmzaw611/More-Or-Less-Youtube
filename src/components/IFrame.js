8500f8da85b0382446f839c7222be233260c6ef5
Kaung Myat Zaw <zaw@purdue.edu>
2021-07-13 13:21:47 +0630

    Loading animation for iframe while it loads Youtube video

--- src/components/IFrame.js
+++ src/components/IFrame.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Segment } from "semantic-ui-react";
 
 const IFrame = ({ videoID, videoTitle }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div>
+    <Segment compact raised loading={isLoading} id="iframe-segment">
       <iframe
         width="512"
         height="288"
         src={`https://www.youtube.com/embed/${videoID}?controls=0`}
         title={videoTitle}
+        onLoad={() => setIsLoading(false)}
       ></iframe>
-    </div>
+    </Segment>
   );
 };
 
